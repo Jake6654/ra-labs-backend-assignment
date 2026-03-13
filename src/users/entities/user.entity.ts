9b95d112a5b3cb27afb44c0e336e8a790e1d3f95
Jake6654 <ugw2ad@virginia.edu>
2026-03-13 13:08:01 -0400

    add user entity and create user dto

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { EventInvite } from 'src/event-invites/entities/event-invite.entity';
+
+@Entity() // defines the User database entity
+export class User {
+  // Auto-generated primary key for each user record.
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  // One user can have many EventInvire rows, each EventInvite row belongs to one user
+  @OneToMany(() => EventInvite, (eventInvite) => eventInvite.user)
+  eventInvites: EventInvite[];
+}
